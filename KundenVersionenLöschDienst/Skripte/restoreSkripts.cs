6fcd2db3f7164884f18eee6d9e5c2afffac1f7f8
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-01-25 16:38:39 +0100

    25/01/2024/16:39

---
+++ KundenVersionenLöschDienst/Skripte/restoreSkripts.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KundenVersionenLöschDienst
+{
+    internal class restoreSkripts
+    {
+        #region Vars & Obj
+
+        private string path = @"";
+        private string[] namesOfSkripts;
+
+        #endregion
+
+        #region Constructors
+
+        public restoreSkripts()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool run()
+        {
+            if (getSkripts())
+            {
+                if (runSkripts())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        private bool getSkripts()
+        {
+            var source = new DirectoryInfo(path);
+
+            var info = (from f in source.GetFiles(path)
+                        where f.Extension == ".sql"
+                        select f);
+
+            int count = 0;
+
+            foreach (FileInfo f in info)
+            {
+                namesOfSkripts[count] = f.Name;
+            }
+
+            return true;
+        }
+
+        private bool runSkripts()
+        {
+
+
+            return false;
+        }
+
+        #endregion
+
+    }
+}
